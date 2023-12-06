161f3039e5fca46e0bee712aac18f1d0c720a840
HABBIE-MARIA-EUGENE <98164451+HABBIE-MARIA-EUGENE@users.noreply.github.com>
2023-12-06 16:43:14 +0530

    Create Distance.c
    With this code we can find the distance between two points in a graph using C language using math.h library

---
+++ Distance.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+
+{
+    int a,b,c,d,e,f;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    scanf("%d",&d);
+    e=(b-a)^2 + (d-c)^2 ;
+    f=sqrt(e);
+    printf("%d",e);
+        return 0;
+}
