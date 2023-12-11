3b07e52d2ccaa1abfaebef37c4b6135f3d3dfaff
HABBIE-MARIA-EUGENE <98164451+HABBIE-MARIA-EUGENE@users.noreply.github.com>
2023-12-11 13:45:48 +0530

    basic_calculator.c

---
+++ basic_calculator.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    
+    int a,b,z;
+    int i;
+    printf("Enter the first number ");
+    scanf("%d",&a);
+    printf("Enter the Second number ");
+    scanf("%d",&b);
+    printf("Select the option \n1=add \n2=subtract\n3=multiply\n4=divide\n");
+    scanf("%d",&i);
+    
+        if (i==1)
+        {
+        z=a+b;
+        printf("%d",z);
+        }    
+        else if (i==2)
+        {
+        z=a-b;
+        printf("%d",z);
+        }
+        else if (i==3)
+        {
+            z=a*b;
+        printf("%d",z);
+        }
+        else if (i==4)
+        {
+        z=a/b;
+        printf("%d",z);
+        }
+        else 
+            printf("ivalid input");
+    
+    return 0;
+    
+}
