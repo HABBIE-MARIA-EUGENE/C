c4504f0b06031e84e57fb2e4e6d127f9023395e6
HABBIE-MARIA-EUGENE <98164451+HABBIE-MARIA-EUGENE@users.noreply.github.com>
2023-12-12 14:38:14 +0530

    basic_calculator.c

--- basic_calculator.c
+++ basic_calculator.c
@@ -1,39 +1,28 @@
 #include<stdio.h>
 int main()
-{
-    
-    int a,b,z;
-    int i;
-    printf("Enter the first number ");
-    scanf("%d",&a);
-    printf("Enter the Second number ");
-    scanf("%d",&b);
-    printf("Select the option \n1=add \n2=subtract\n3=multiply\n4=divide\n");
-    scanf("%d",&i);
-    
-        if (i==1)
+{   
+    int a,b,c;
+    char op;
+    printf("enter the values along with the operator ");
+    scanf("%d%c%d",&a,&op,&b);
+    switch(op)
         {
-        z=a+b;
-        printf("%d",z);
-        }    
-        else if (i==2)
-        {
-        z=a-b;
-        printf("%d",z);
-        }
-        else if (i==3)
-        {
-            z=a*b;
-        printf("%d",z);
-        }
-        else if (i==4)
-        {
-        z=a/b;
-        printf("%d",z);
-        }
-        else 
+        case '+':
+            c=a+b;
+            break;
+        case '-':
+            c=a-b;
+            break;
+        case '*':
+            c=a*b;
+            break;    
+        case '/':
+            c=a/b;
+            break;
+            default:
             printf("invalid input");
+        }
+        printf("%d",c);
     
-    return 0;
-    
+     return 0;
 }
